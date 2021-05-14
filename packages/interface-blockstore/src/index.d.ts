21980fe533dc03ef2e1303058ca1c5cf5a67385a
achingbrain <alex@achingbrain.net>
2021-05-14 12:24:19 +0100

    chore: build before publish and export all blockstore types

--- packages/interface-blockstore/src/index.d.ts
+++ packages/interface-blockstore/src/index.d.ts
@@ -1,10 +1,50 @@
 import {
-  Pair as StorePair
+  Pair as StorePair,
+  Batch as StoreBatch,
+  QueryFilter as StoreQueryFilter,
+  QueryOrder as StoreQueryOrder,
+  Query as StoreQuery,
+  KeyQueryFilter as StoreKeyQueryFilter,
+  KeyQueryOrder as StoreKeyQueryOrder,
+  KeyQuery as StoreKeyQuery,
+  Store
 } from 'interface-store'
 import {
   CID
 } from 'multiformats'
 
 export interface Pair extends StorePair<CID, Uint8Array> {
+
+}
+
+export interface Batch extends StoreBatch<CID, Uint8Array> {
+
+}
+
+export interface Blockstore extends Store<CID, Uint8Array> {
+
+}
+
+export interface QueryFilter extends StoreQueryFilter<CID, Uint8Array> {
+
+}
+
+export interface QueryOrder extends StoreQueryOrder<CID, Uint8Array> {
+
+}
+
+export interface Query extends StoreQuery<CID, Uint8Array> {
+
+}
+
+export interface KeyQueryFilter extends StoreKeyQueryFilter<CID> {
+
+}
+
+export interface KeyQueryOrder extends StoreKeyQueryOrder<CID> {
+
+}
+
+export interface KeyQuery extends StoreKeyQuery<CID> {
 
 }
