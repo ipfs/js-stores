62a2417e1330d1c49c0a70453e02bd878e0b3dd8
achingbrain <alex@achingbrain.net>
2023-03-13 10:44:23 +0100

    feat!: simplify store interface
    - Removes open/close from store as not every implementation needs them
    - Removes query from store as it doesn't make sense for blockstores
    - Removes batch from store as storing blocks in memory before writing is a bad idea
    - Adds query/batch to datastore interface
    - Adds getAll to blockstore interface
    - Converts datastore tests to typescript
    - Improves typing of datastore/blockstore tests to allow setup/teardown to recieve the right store type to open/close, etc
    
    BREAKING CHANGE: store interface has fewer methods, datastore has query/batch, blockstore has getAll methods

--- packages/interface-blockstore/src/index.ts
+++ packages/interface-blockstore/src/index.ts
@@ -1,13 +1,6 @@
 import type {
-  Pair as StorePair,
-  Batch as StoreBatch,
-  QueryFilter as StoreQueryFilter,
-  QueryOrder as StoreQueryOrder,
-  Query as StoreQuery,
-  KeyQueryFilter as StoreKeyQueryFilter,
-  KeyQueryOrder as StoreKeyQueryOrder,
-  KeyQuery as StoreKeyQuery,
   Options as StoreOptions,
+  AwaitIterable,
   Store
 } from 'interface-store'
 import type {
@@ -18,38 +11,22 @@ export interface Options extends StoreOptions {
 
 }
 
-export interface Pair extends StorePair<CID, Uint8Array> {
-
+export interface Pair {
+  cid: CID
+  block: Uint8Array
 }
 
-export interface Batch extends StoreBatch<CID, Uint8Array> {
-
-}
-
-export interface Blockstore extends Store<CID, Uint8Array> {
-
-}
-
-export interface QueryFilter extends StoreQueryFilter<CID, Uint8Array> {
-
-}
-
-export interface QueryOrder extends StoreQueryOrder<CID, Uint8Array> {
-
-}
-
-export interface Query extends StoreQuery<CID, Uint8Array> {
-
-}
-
-export interface KeyQueryFilter extends StoreKeyQueryFilter<CID> {
-
-}
-
-export interface KeyQueryOrder extends StoreKeyQueryOrder<CID> {
-
-}
-
-export interface KeyQuery extends StoreKeyQuery<CID> {
-
+export interface Blockstore extends Store<CID, Uint8Array, Pair> {
+  /**
+   * Retrieve all cid/block pairs from the blockstore as an unordered iterable
+   *
+   * @example
+   * ```js
+   * for await (const { cid, block } of store.getAll()) {
+   *   console.log('got:', cid, block)
+   *   // => got CID('Qmfoo') Uint8Array[...]
+   * }
+   * ```
+   */
+  getAll: (options?: Options) => AwaitIterable<Pair>
 }
