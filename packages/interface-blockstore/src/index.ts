e64bd41f1e8f627e6ff8870cf98333c702506753
achingbrain <alex@achingbrain.net>
2023-06-07 07:11:38 +0100

    chore: add note

--- packages/interface-blockstore/src/index.ts
+++ packages/interface-blockstore/src/index.ts
@@ -1,4 +1,8 @@
 /* eslint-disable @typescript-eslint/ban-types */
+// this ignore is so we can use {} as the default value for the options
+// extensions below - it normally means "any non-nullish value" but here
+// we are using it as an intersection type - see the aside at the bottom:
+// https://github.com/typescript-eslint/typescript-eslint/issues/2063#issuecomment-675156492
 
 import type {
   AbortOptions,
