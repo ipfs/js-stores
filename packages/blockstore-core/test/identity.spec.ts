3d84dd0ab164fb5749c34487a217c763d1d09ccb
Alex Potsides <alex@achingbrain.net>
2024-04-09 19:33:45 +0100

    fix: identity blockstore should wrap child (#303)
    Allow passing a child blockstore to an identity blockstore.

--- packages/blockstore-core/test/identity.spec.ts
+++ packages/blockstore-core/test/identity.spec.ts
@@ -1,19 +1,23 @@
 /* eslint-env mocha */
 
 import { expect } from 'aegir/chai'
+import all from 'it-all'
 import drain from 'it-drain'
 import { CID } from 'multiformats/cid'
 import * as raw from 'multiformats/codecs/raw'
 import { identity } from 'multiformats/hashes/identity'
 import { sha256 } from 'multiformats/hashes/sha2'
 import { IdentityBlockstore } from '../src/identity.js'
+import { MemoryBlockstore } from '../src/memory.js'
 import type { Blockstore } from 'interface-blockstore'
 
 describe('identity', () => {
   let blockstore: Blockstore
+  let child: Blockstore
 
   beforeEach(() => {
     blockstore = new IdentityBlockstore()
+    child = new MemoryBlockstore()
   })
 
   it('has an identity CID', () => {
@@ -55,5 +59,71 @@ describe('identity', () => {
     await drain(blockstore.deleteMany([cid]))
 
     expect(blockstore.has(cid)).to.be.true()
+  })
+
+  it('puts CIDs to child', async () => {
+    const block = Uint8Array.from([0, 1, 2, 3, 4])
+    const multihash = await sha256.digest(block)
+    const cid = CID.createV1(raw.code, multihash)
+
+    blockstore = new IdentityBlockstore(child)
+
+    await blockstore.put(cid, block)
+    expect(child.has(cid)).to.be.true()
+    expect(child.get(cid)).to.equalBytes(block)
+  })
+
+  it('gets CIDs from child', async () => {
+    const block = Uint8Array.from([0, 1, 2, 3, 4])
+    const multihash = await sha256.digest(block)
+    const cid = CID.createV1(raw.code, multihash)
+
+    await child.put(cid, block)
+
+    blockstore = new IdentityBlockstore(child)
+    expect(blockstore.has(cid)).to.be.true()
+    expect(blockstore.get(cid)).to.equalBytes(block)
+  })
+
+  it('has CIDs from child', async () => {
+    const block = Uint8Array.from([0, 1, 2, 3, 4])
+    const multihash = await sha256.digest(block)
+    const cid = CID.createV1(raw.code, multihash)
+
+    await child.put(cid, block)
+
+    blockstore = new IdentityBlockstore(child)
+    expect(blockstore.has(cid)).to.be.true()
+  })
+
+  it('deletes CIDs from child', async () => {
+    const block = Uint8Array.from([0, 1, 2, 3, 4])
+    const multihash = await sha256.digest(block)
+    const cid = CID.createV1(raw.code, multihash)
+
+    await child.put(cid, block)
+
+    blockstore = new IdentityBlockstore(child)
+    expect(blockstore.has(cid)).to.be.true()
+
+    await blockstore.delete(cid)
+
+    expect(blockstore.has(cid)).to.be.false()
+  })
+
+  it('gets all pairs from child', async () => {
+    const block = Uint8Array.from([0, 1, 2, 3, 4])
+    const multihash = await sha256.digest(block)
+    const cid = CID.createV1(raw.code, multihash)
+
+    await child.put(cid, block)
+
+    blockstore = new IdentityBlockstore(child)
+    expect(blockstore.has(cid)).to.be.true()
+
+    const result = await all(blockstore.getAll())
+
+    expect(result).to.have.lengthOf(1)
+    expect(result[0].cid.toString()).to.equal(cid.toString())
   })
 })
