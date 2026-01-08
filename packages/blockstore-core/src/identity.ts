9ec868eb2c541803deb70ba253e2d10768cf7355
tabcat <tabcat00@proton.me>
2026-01-08 08:30:42 -0600

    fix lint errors

--- packages/blockstore-core/src/identity.ts
+++ packages/blockstore-core/src/identity.ts
@@ -1,7 +1,7 @@
 import { NotFoundError } from 'interface-store'
 import { BaseBlockstore } from './base.js'
 import type { Blockstore, Pair } from 'interface-blockstore'
-import type { AbortOptions, Await, AwaitGenerator, AwaitIterable } from 'interface-store'
+import type { AbortOptions, Await, AwaitIterable } from 'interface-store'
 import type { CID } from 'multiformats/cid'
 
 // https://github.com/multiformats/multicodec/blob/d06fc6194710e8909bac64273c43f16b56ca4c34/table.csv#L2
